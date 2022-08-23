0f55bee9e6c387498ec953bf14affa18d0000814
iamngoni <imngonii@gmail.com>
2022-08-23 18:08:34 +0200

    feat: window_size plugin

--- dhra/windows/flutter/generated_plugin_registrant.cc
+++ dhra/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <auto_updater/auto_updater_plugin.h>
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AutoUpdaterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AutoUpdaterPlugin"));
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
--- dhra/windows/flutter/generated_plugins.cmake
+++ dhra/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  auto_updater
+  bitsdojo_window_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
