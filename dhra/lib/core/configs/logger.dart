f73912893658b01205edfe8afae58d28e99e393b
iamngoni <imngonii@gmail.com>
2022-09-16 21:53:14 +0200

    feat: configure logger

--- dhra/lib/core/configs/logger.dart
+++ dhra/lib/core/configs/logger.dart
@@ -1,28 +1,28 @@
 /*
     @Project: hekima_patient
-    @Date: 10, September, 2022
+    @Date: 16, September, 2022
     @Author: Ngonidzashe Mangudya
 
     Copyright (c) 2022 ModestNerd
 */
 
-// import 'package:logger/logger.dart';
-//
-// final Logger logger = Logger(
-//   printer: PrettyPrinter(
-//     printTime: true,
-//   ),
-// );
-//
-// extension AppLogger on Logger {
-//   void verbose(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
-//       v(message, error, stackTrace);
-//   void debug(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
-//       d(message, error, stackTrace);
-//   void info(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
-//       i(message, error, stackTrace);
-//   void warning(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
-//       w(message, error, stackTrace);
-//   void error(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
-//       e(message, error, stackTrace);
-// }
+import 'package:logger/logger.dart';
+
+final Logger logger = Logger(
+  printer: PrettyPrinter(
+    printTime: true,
+  ),
+);
+
+extension AppLogger on Logger {
+  void verbose(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
+      v(message, error, stackTrace);
+  void debug(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
+      d(message, error, stackTrace);
+  void info(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
+      i(message, error, stackTrace);
+  void warning(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
+      w(message, error, stackTrace);
+  void error(dynamic message, [dynamic error, StackTrace? stackTrace]) =>
+      e(message, error, stackTrace);
+}
