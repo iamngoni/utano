b95a05fe9d99956aab9c382dd19f926f86053fa8
Ngonidzashe Mangudya <imngonii@gmail.com>
2022-12-23 01:26:57 +0200

    feat: setup urls

--- dhra_api/utano/urls.py
+++ dhra_api/utano/urls.py
@@ -1,21 +1,20 @@
-"""dhra_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+  path("admin", admin.site.urls),
+  path("jobs", include("django_rq.urls")),
+  path("api/1.0/", include("api.urls")),
+  path(
+      "docs",
+      get_schema_view(
+          title="Utano API",
+          description="Utano REST API",
+          version="1.0",
+      ),
+      name="open-api schema",
+  ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
