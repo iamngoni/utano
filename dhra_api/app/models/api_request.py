076cca3b7763b8aeb245a4b85e332bf754b2a796
Ngonidzashe Mangudya <imngonii@gmail.com>
2022-12-23 00:21:20 +0200

    fix: revert to django

--- dhra_api/app/models/api_request.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class ApiRequest(BaseModel):
-    method: str
-    headers: str
-    path: str
--- dhra_api/app/routers/users.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/", tags=["users"])
-def read_root():
-    return {"Hello": "World"}
