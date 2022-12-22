54e9a001fcc9acf502af15a1350546148da7042f
Ngonidzashe Mangudya <imngonii@gmail.com>
2022-12-23 00:26:02 +0200

    fix: rename main app to utano

--- dhra_api/utano/asgi.py
+++ dhra_api/utano/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dhra_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'utano.settings')
 
 application = get_asgi_application()
