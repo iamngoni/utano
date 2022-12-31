b80ecfbcc854b08d764da7004b4e1abe003cee20
Ngonidzashe Mangudya <imngonii@gmail.com>
2022-12-31 08:36:04 +0200

    fix: move schema view docs to api/urls.py

--- dhra_api/api/urls.py
+++ dhra_api/api/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.renderers import CoreJSONRenderer
+
 
 urlpatterns = [
     path("auth/", include("api.views.auth.urls")),
+    path("health_institute/", include("api.views.health_institution.urls")),
+    path(
+        "docs",
+        get_schema_view(
+            title="Utano REST API",
+            description="Utano REST API",
+            version="1.0",
+            renderer_classes=(CoreJSONRenderer,),
+        ),
+        name="Utano REST API",
+    ),
 ]
--- dhra_api/utano/urls.py
+++ dhra_api/utano/urls.py
@@ -2,19 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-  path("admin", admin.site.urls),
-  path("jobs", include("django_rq.urls")),
-  path("api/1.0/", include("api.urls")),
-  path(
-      "docs",
-      get_schema_view(
-          title="Utano API",
-          description="Utano REST API",
-          version="1.0",
-      ),
-      name="open-api schema",
-  ),
+    path("admin", admin.site.urls),
+    path("jobs", include("django_rq.urls")),
+    path("api/1.0/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
