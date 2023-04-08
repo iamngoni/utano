b9c90da6ffcfe872434bcf20b5838bc34650b370
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-04-08 20:34:09 +0200

    feat: display correct system statistics for system admin

--- dhra/lib/core/views/widgets/dashboard_stat_widget.dart
+++ dhra/lib/core/views/widgets/dashboard_stat_widget.dart
@@ -7,93 +7,115 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:relative_scale/relative_scale.dart';
 
+import '../../../system_admin/blocs/system_stats/system_stats_bloc.dart';
 import '../../configs/colors.dart';
+import 'loader_widget.dart';
 
 class DashboardStatWidget extends StatelessWidget {
   const DashboardStatWidget({
     required this.title,
     required this.assetImage,
     required this.number,
+    this.isLoading = false,
     super.key,
   });
 
   final String title;
   final AssetImage assetImage;
-  final double number;
+  final String number;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
     return RelativeBuilder(
       builder: (context, height, width, sy, sx) {
-        return Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: sx(7),
-            vertical: sy(15),
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: UtanoColors.border,
+        return GestureDetector(
+          onTap: () => context.read<SystemStatsBloc>().add(GetStatistics()),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: sx(7),
+              vertical: sy(15),
             ),
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: UtanoColors.grey.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 6,
-                offset: const Offset(0.5, 0.5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: UtanoColors.border,
               ),
-              BoxShadow(
-                color: UtanoColors.grey.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 1,
-                offset: const Offset(-0.5, -0.5),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: UtanoColors.inactive.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: UtanoColors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 6,
+                  offset: const Offset(0.5, 0.5),
                 ),
-                child: ImageIcon(
-                  assetImage,
-                  color: UtanoColors.black,
+                BoxShadow(
+                  color: UtanoColors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: const Offset(-0.5, -0.5),
                 ),
-              ),
-              SizedBox(
-                width: sx(5),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      color: UtanoColors.grey,
-                      fontWeight: FontWeight.w400,
-                      fontSize: sy(9),
+              ],
+            ),
+            child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              child: !isLoading
+                  ? Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: UtanoColors.inactive.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ImageIcon(
+                            assetImage,
+                            color: UtanoColors.black,
+                          ),
+                        ),
+                        SizedBox(
+                          width: sx(5),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              title,
+                              style: TextStyle(
+                                color: UtanoColors.grey,
+                                fontWeight: FontWeight.w400,
+                                fontSize: sy(9),
+                              ),
+                            ),
+                            SizedBox(
+                              height: sy(3),
+                            ),
+                            Text(
+                              number,
+                              style: TextStyle(
+                                color: UtanoColors.black,
+                                fontWeight: FontWeight.w600,
+                                fontSize: sy(12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  : Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: sy(13),
+                        ),
+                        child: const LoaderWidget(
+                          color: UtanoColors.black,
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: sy(3),
-                  ),
-                  Text(
-                    number.toStringAsFixed(0),
-                    style: TextStyle(
-                      color: UtanoColors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: sy(12),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
         );
       },
--- dhra/lib/core/views/widgets/dashboard_stats_widget.dart
+++ dhra/lib/core/views/widgets/dashboard_stats_widget.dart
@@ -7,8 +7,10 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:relative_scale/relative_scale.dart';
 
+import '../../../system_admin/blocs/system_stats/system_stats_bloc.dart';
 import 'dashboard_stat_widget.dart';
 
 class DashboardStatsWidget extends StatelessWidget {
@@ -20,46 +22,60 @@ class DashboardStatsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return RelativeBuilder(
       builder: (context, height, width, sy, sx) {
-        return Row(
-          children: [
-            const Expanded(
-              child: DashboardStatWidget(
-                title: 'Health Institutions',
-                assetImage: AssetImage('assets/icons/user-add.png'),
-                number: 1250,
-              ),
-            ),
-            SizedBox(
-              width: sx(3),
-            ),
-            const Expanded(
-              child: DashboardStatWidget(
-                title: 'Employees',
-                assetImage: AssetImage('assets/icons/user-add.png'),
-                number: 500,
-              ),
-            ),
-            SizedBox(
-              width: sx(3),
-            ),
-            const Expanded(
-              child: DashboardStatWidget(
-                title: 'Patients',
-                assetImage: AssetImage('assets/icons/user-add.png'),
-                number: 58,
-              ),
-            ),
-            SizedBox(
-              width: sx(3),
-            ),
-            const Expanded(
-              child: DashboardStatWidget(
-                title: 'Total Doctors',
-                assetImage: AssetImage('assets/icons/user-add.png'),
-                number: 5,
-              ),
-            ),
-          ],
+        return BlocBuilder<SystemStatsBloc, SystemStatsState>(
+          builder: (context, state) {
+            return Row(
+              children: [
+                Expanded(
+                  child: DashboardStatWidget(
+                    title: 'Health Institutions',
+                    assetImage: const AssetImage('assets/icons/user-add.png'),
+                    number: state is SystemStatsLoaded
+                        ? state.stats.healthInstitutions.toStringAsFixed(0)
+                        : 'n/a',
+                    isLoading: state is SystemStatsLoading,
+                  ),
+                ),
+                SizedBox(
+                  width: sx(3),
+                ),
+                Expanded(
+                  child: DashboardStatWidget(
+                    title: 'Employees',
+                    assetImage: const AssetImage('assets/icons/user-add.png'),
+                    number: state is SystemStatsLoaded
+                        ? state.stats.employees.toStringAsFixed(0)
+                        : 'n/a',
+                    isLoading: state is SystemStatsLoading,
+                  ),
+                ),
+                SizedBox(
+                  width: sx(3),
+                ),
+                Expanded(
+                  child: DashboardStatWidget(
+                    title: 'Patients',
+                    assetImage: const AssetImage('assets/icons/user-add.png'),
+                    number: state is SystemStatsLoaded
+                        ? state.stats.patients.toStringAsFixed(0)
+                        : 'n/a',
+                    isLoading: state is SystemStatsLoading,
+                  ),
+                ),
+                SizedBox(
+                  width: sx(3),
+                ),
+                Expanded(
+                  child: DashboardStatWidget(
+                    title: 'To Be Decided',
+                    assetImage: const AssetImage('assets/icons/user-add.png'),
+                    number: 'n/a',
+                    isLoading: state is SystemStatsLoading,
+                  ),
+                ),
+              ],
+            );
+          },
         );
       },
     );
